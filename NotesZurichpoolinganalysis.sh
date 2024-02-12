07a78ab7234a18423852b4e3c5ee1d3d13a58ee6
RhalenaThomas <rhalena.t@gmail.com>
2024-02-12 13:13:31 -0500

    check out annotations for new demultiplexing

---
+++ NotesZurichpoolinganalysis.sh
@@ -0,0 +1,12 @@
+# Zurich pooling analysis 
+
+# Michael's pipeline results
+
+cd /lustre04/scratch/fiorini9/mjff/ADHD/pool3
+cd /lustre04/scratch/fiorini9/mjff/ADHD/pool1/step7
+
+# results directories
+
+cd /lustre04/scratch/fiorini9/mjff/ADHD/FCN_step7
+cd /lustre04/scratch/fiorini9/mjff/ADHD/NSC_step7
+
